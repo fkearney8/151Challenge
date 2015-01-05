8184f66fa73265f56acea4cdd21cccf39c84742d
Frank Kearney <fkearney8@gmail.com>
2015-01-04 23:17:51 -0800

    Started adding my own pages

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,14 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("151 Challenge"))
   }
 
+  def recordExercise= Action {
+    Ok(views.html.recordExercise())
+  }
+
+  def displayEntries = Action {
+    Ok(views.html.displayEntries())
+  }
 }
