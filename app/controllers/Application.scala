8832a3038b8d3dc2d792cfd4d99d44acee631bc1
Frank Kearney <fkearney8@gmail.com>
2015-01-08 00:40:24 -0800

    Authenticating users. Currently authenticate as Theresa if you go to the View Progress page. /showAuth and /testAuth pages allowed testing that it worked. Next will decide how to pass user to templates, using implicits doesn't seem to work properly.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,17 +1,28 @@
+
 package controllers
 
 import models.Users
 import play.api._
 import play.api.libs.json.Json
+import play.api.mvc.Security.AuthenticatedBuilder
 import play.api.mvc._
+import play.twirl.api.HtmlFormat
 
 object Application extends Controller {
 
+  private val USERNAME_SESSION_KEY: String = "username"
+
+  def getAuthenticatedUser(request: RequestHeader): Option[String] = {
+    request.session.get(USERNAME_SESSION_KEY)
+  }
+
   def index = Action {
+    implicit val user = Option("hi")
     Ok(views.html.index())
   }
 
   def recordExercise= Action {
+
     Ok(views.html.recordExercise())
   }
 
@@ -19,12 +30,37 @@ object Application extends Controller {
     Ok(views.html.displayEntries())
   }
 
-  def viewProgress = Action {
+  def viewProgress = Action { request =>
     Ok(views.html.viewProgress())
+        //TODO cheating for now and just logging in as 'Theresa' when someone goes to the view progress page
+        .withSession(request.session + (USERNAME_SESSION_KEY, "Theresa"))
   }
 
-  def login() = Action {
+  def login() = Action { implicit request =>
     val usersList = Users.getUsers
-    Ok(views.html.loginPage() + usersList.toString())
+    Ok(views.html.loginPage(usersList))
   }
+
+  def logout() = Action { implicit request =>
+    Redirect("/login").withNewSession
+  }
+
+  def isAuthenticated(f: => String => Request[AnyContent] => Result): EssentialAction = {
+    def userInfo(rh: RequestHeader): Option[String] = { rh.session.get(USERNAME_SESSION_KEY) }
+    def onUnauthorized(rh: RequestHeader): Result = { Redirect("/login") }
+    Security.Authenticated(userInfo, onUnauthorized) { user =>
+      Action(request => f(user)(request))
+    }
+  }
+
+  //then in a controller
+  def authRequiredTestPage: EssentialAction = isAuthenticated { username => request =>
+    Ok("This page required Auth, Hello " + username)
+  }
+
+  def showAuthPage = Action { request =>
+    Ok(views.html.titlebar(getAuthenticatedUser(request)))
+  }
+
+
 }
