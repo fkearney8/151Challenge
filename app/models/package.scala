eb54bc553d7b174453b7afe363f90e6f19d86d2a
castle8080 <castillo.bryan@gmail.com>
2015-01-12 09:40:00 -0800

    Adding back Alpha Chen's change to database connections for testing.

--- app/models/package.scala
+++ app/models/package.scala
@@ -5,7 +5,12 @@ package object models {
   import play.api.Logger
   import play.api.Play.current
 
-  lazy val db = {
+  // TODO: I think we should cache the Database object on app startup
+  //       and destroy it when the app shuts down, instead of using
+  //       a global connection object.
+  //       If you make this a lazy val it doesn't actually get shutdown
+  //       right when running tests.
+  val db = {
     Logger.debug(s"Configuring slick database: $DB")
     Database.forDataSource(DB.getDataSource())
   }
