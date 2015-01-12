d7164d363a7cb85287bb31dac7ce811c30fefd97
castle8080 <castillo.bryan@gmail.com>
2015-01-12 07:25:18 -0800

    Login script can be run from other directories.

--- scripts/logIntoRemoteHost.sh
+++ scripts/logIntoRemoteHost.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-
-ssh -l ubuntu -i ../private/OscarPartyPrivateKey.pem ec2-54-165-191-142.compute-1.amazonaws.com
-
+script_dir="$(dirname $0)"
+exec ssh -l ubuntu -i "${script_dir}/../private/OscarPartyPrivateKey.pem" ec2-54-165-191-142.compute-1.amazonaws.com
