c4f1d017cbecf78ca491de5c74406f8cde31a89f
Frank Kearney <fkearney8@gmail.com>
2017-01-16 22:41:07 -0800

    Updated exercises and DB for 2017

--- app/utils/ChallengeYears.scala
+++ app/utils/ChallengeYears.scala
@@ -4,7 +4,8 @@ object ChallengeYears {
   sealed abstract class ChallengeYear(val yearString: String, val entriesTable: String)
 
   case object Year2015 extends ChallengeYear("2015", "EXERCISE_ENTRIES_2015")
-  case object ThisYear extends ChallengeYear("2016", "EXERCISE_ENTRIES_2016")
+  case object Year2016 extends ChallengeYear("2016", "EXERCISE_ENTRIES_2016")
+  case object ThisYear extends ChallengeYear("2017", "EXERCISE_ENTRIES_2017")
 
 }
 
--- app/utils/Exercises.scala
+++ app/utils/Exercises.scala
@@ -10,14 +10,15 @@ object Exercises {
   def exercisesForYear(year: ChallengeYear = ThisYear) = {
     year match {
       case Year2015 => (SitUps, Lunges, Burpees, Miles, Shot)
-      case ThisYear => (AirSquats, PushUps, Planks, Miles, Shot)
+      case Year2016 => (AirSquats, PushUps, Planks, Miles, Shot)
+      case ThisYear => (MountainClimbers, TricepDips, VeeUps, Miles, Shot)
     }
   }
 
   def exercisesForYearList(year: ChallengeYear = ThisYear): List[Exercise] =
       List(exercisesForYear(year)._1, exercisesForYear(year)._2, exercisesForYear(year)._3, exercisesForYear(year)._4, exercisesForYear(year)._5)
 
-  private val allExercises: Seq[Exercise] = List(SitUps, Lunges, Burpees, Miles, Shot, PushUps, AirSquats, Planks)
+  private val allExercises: Seq[Exercise] = List(SitUps, Lunges, Burpees, Miles, Shot, PushUps, AirSquats, Planks, MountainClimbers, TricepDips, VeeUps)
 
 
   sealed abstract class Exercise(val name: String, val displayName: String)
@@ -30,15 +31,19 @@ object Exercises {
     }
   }
 
+  case object Miles extends Exercise("Miles", "Miles")
+  case object Shot extends Exercise("Shot", "Shot")
+
   case object SitUps extends Exercise("SitUps", "Sit Ups")
   case object Lunges extends Exercise("Lunges", "Lunges")
   case object Burpees extends Exercise("Burpees", "Burpees")
 
-  case object Miles extends Exercise("Miles", "Miles")
-  case object Shot extends Exercise("Shot", "Shot")
-
   case object PushUps extends Exercise("PushUps", "Push Ups")
   case object AirSquats extends Exercise("AirSquats", "Air Squats")
   case object Planks extends Exercise("Planks", "Planks")
+  
+  case object MountainClimbers extends Exercise("MountainClimbers", "Mountain Climbers")
+  case object TricepDips extends Exercise("TricepDips", "Tricep Dips")
+  case object VeeUps extends Exercise("VeeUps", "V-Ups")
 }
 
